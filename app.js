beb472685e1bdad9baf127a6a5fdc694b911afef
HowardWu5566 <ggg1237890@gmail.com>
2022-09-13 10:38:48 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const renderName = require('./utils/check')
 
 app.use(express.urlencoded({ extended: true }))
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -16,7 +23,6 @@ app.post('/login', (req, res) => {
   const { email, password } = req.body
   const userFirstName = renderName(email, password)
   const errorMessage = 'Email 或Password 錯誤'
-  console.log(userFirstName)
   userFirstName ? res.render('welcome', { userFirstName }) : res.render('index', { errorMessage })
 })
 
